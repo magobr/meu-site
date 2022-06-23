e27648c3f4b95781079d513ee30765415d98c688
Thiago Novaes <novaesthago@gmail.com>
2022-06-23 20:42:48 -0300

    Ajuste

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 use Pecee\SimpleRouter\SimpleRouter;
-use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
 
 require_once './src/routes.php';
 
