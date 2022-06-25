48069ae8f61b1c7fcefbe338b701ad2fa494d0a5
Thiago Novaes <novaesthago@gmail.com>
2022-06-25 14:18:42 -0300

    Test Comentários

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,5 +1,6 @@
 const sectionContainer = document.querySelector("section .container");
 const sectionContainerPost = document.querySelector("section .container .content__posts");
+const divComentarios = document.querySelector('#disqus_thread');
 
 const consumeApi = async (body=null, endPoint, method="POST", mode="") =>{
     var dataForm = new FormData();
@@ -39,7 +40,6 @@ const componentAllPosts = (content) => {
 }
 
 const componentPost = (content) => {
-    console.log(content);
     return `<div id="${content.id}" class="content__posts__item">
                 <button id="back_post">Voltar</button>
                 <div class="posts__item">
@@ -74,6 +74,13 @@ async function loadAllPosts(){
             sectionContainerPost.remove();
 
             sectionContainer.innerHTML = componentPost(res.data[0])
+            divComentarios.classList.remove('d-none');
+
+            var disqus_config = function () {
+                this.page.url = window.location;
+                this.page.identifier = data[0].id; 
+            };
+            
             let btnBack = document.getElementById("back_post");
             btnBack.addEventListener("click", async ()=>{
                 window.location.reload();
