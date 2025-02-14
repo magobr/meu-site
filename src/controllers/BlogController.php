eb07d391e7a821170f6bdf475183d53c1246251e
Thiago Novaes <novaesthago@gmail.com>
2025-02-14 19:11:56 -0300

    reaftc: melhorando o codigo: adicionando serviço para as rotas que redenriza dados do banco

--- src/controllers/BlogController.php
+++ src/controllers/BlogController.php
@@ -88,31 +88,6 @@ class BlogController extends PostsModel
       ]);
    }
 
-   static public function viewPost($id)
-   {
-      $result = PostsModel::findPostsById($id);
-
-      if (isset($result['error']) && $result['error']) {
-         return [
-            "error" => true,
-            "data" => $result,
-            "message" => "Erro ao buscar dados"
-        ];
-      }
-
-      if (isset($result) && sizeof($result) === 0) {
-         return [
-            "error" => false,
-            "data" => $result,
-            "message" => "Não foi encontrado dados"
-        ];
-      }
-      return [
-         "error" => false,
-         "data" => $result[0]
-      ];
-   }
-
    static public function getPostByUser($id)
    {
       $result = PostsModel::findPostsByUser($id);
@@ -137,32 +112,6 @@ class BlogController extends PostsModel
          "error" => false,
          "data" => $result
      ]);
-   }
-
-   static public function viewPostByUser($id)
-   {
-      $result = PostsModel::findPostsByUser($id);
-
-      if (isset($result['error']) && $result['error']) {
-         return [
-            "error" => true,
-            "data" => $result,
-            "message" => "Erro ao buscar dados"
-        ];
-      }
-
-      if (sizeof($result) === 0) {
-         return [
-            "error" => true,
-            "data" => $result,
-            "message" => "Não foi encontrado dados"
-        ];
-      }
-      
-      return [
-         "error" => false,
-         "data" => $result
-      ];
    }
 
    static public function insertPost()
